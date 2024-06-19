58bf387ec82646d3fa9fc9607c91a96d0c4e4391
Ju-code12 <idowujulia56@gmail.com>
2024-06-19 19:24:06 +0100

    julia

---
+++ Kestrel.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+  var menu = document.getElementById('navbar-menu');
+  menu.classList.toggle('active');
+}
+
+function redirectToNewPage() {
+  window.location.href = 'kestrelnew.html';
+}
+
+
+
+
+
+
+
+
+
